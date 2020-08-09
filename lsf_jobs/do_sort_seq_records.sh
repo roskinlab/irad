b8904f453f38bf0c4c9dbd9f781246fc8fce6634
Krishna Roskin <krishnaroskin@gmail.com>
2020-08-09 14:13:41 -0400

    sequence record sorters now sort into HIVE style Avro direcotry for futher processing instead of converting into Parquet

--- lsf_jobs/do_sort_seq_records.sh
+++ lsf_jobs/do_sort_seq_records.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/bash
 
-CONVERTER=/data/RoskinLab/irbase/pipeline/seq_record_to_parquet.py
-SUBJECT_LIST=/data/RoskinLab/irbase/lsf_jobs/subjects.sh
-
-SOURCE_DIR=${1?direcotry with the Avro files must be given}
-
-LABEL=$(basename "${SOURCE}")
+SORTER=~/irbase/pipeline/sort_seq_records.py
+DATA_DIR=${1?directory to store dataset has not been set}
+shift
 
 cat <<EOF
 #BSUB -L /bin/bash
-#BSUB -W 8:00
-#BSUB -M 16000
-#BSUB -J Parquet_convert
+#BSUB -W 16:00
+#BSUB -M 30000
+#BSUB -J sort_${DATA_DIR}
+#BSUB -o logs/sort_${DATA_DIR}_%J.log
 
+~/irbase/pipeline/sort_seq_records.py ${DATA_DIR} $@
 EOF
-for s in $(${SUBJECT_LIST} ${SOURCE_DIR}) ; do
-    echo ${CONVERTER} \"subjects/${s}.avro\"
-done
--- pipeline/sort_seq_records.py
+++ pipeline/sort_seq_records.py
@@ -6,35 +6,118 @@ import sys
 import argparse
 import logging
 import time
+import os
+import tempfile
+from operator import itemgetter
 
 import fastavro
+from fastavro.io.binary_encoder import BinaryEncoder
+from fastavro.io.json_encoder import AvroJSONEncoder
+from fastavro.read import SYNC_SIZE
+from fastavro.write import Writer
 
 from roskinlib.utils import open_compressed
 
+def open_avro(fo, schema, codec='null', sync_interval=1000 * SYNC_SIZE, metadata=None,
+              validator=None, sync_marker=None, codec_compression_level=None):
+    if isinstance(fo, AvroJSONEncoder):
+        writer_class = JSONWriter
+    else:
+        # Assume a binary IO if an encoder isn't given
+        writer_class = Writer
+        fo = BinaryEncoder(fo)
+
+    output = writer_class(fo,
+                          schema,
+                          codec,
+                          sync_interval,
+                          metadata,
+                          validator,
+                          sync_marker,
+                          codec_compression_level)
+    return output
 
 def main():
-    parser = argparse.ArgumentParser(description='sort the sequence records in the given Avro file',
+    parser = argparse.ArgumentParser(description='sort the sequence records in the given Avro file into a HIVE style directory structure',
             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     # input files
-    parser.add_argument('seq_record_filename', metavar='seq_record.avro', help='the Avro file with the sequence records')
+    parser.add_argument('pathname_base', metavar='dir_path', help='the base pathname for the HIVE style directory structure')
+    parser.add_argument('seq_record_filenames', metavar='seq_rec.avro', nargs='+', help='the Avro file with the sequence records')
+    # options
+    arg_group = parser.add_mutually_exclusive_group(required=False)
+    arg_group.add_argument('--no-none', action='store_true', help='do not process records without a subject')
+    arg_group.add_argument('--only-none', action='store_true', help='only process records without a subject')
 
     args = parser.parse_args()
     logging.basicConfig(level=logging.INFO)
     start_time = time.time()
+
+    # make the base directory if it doesn't already exist
+    base_path = args.pathname_base
+    if os.path.isdir(base_path):
+        logging.info('using existing base directory %s and adding to it', base_path)
+    else:
+        logging.info('making base directory %s', base_path)
+        os.mkdir(base_path)
     
-    with open_compressed(args.seq_record_filename, 'rb') as seq_record_handle:
-        seq_record_reader = fastavro.reader(seq_record_handle)
+    # load the records in and group them by subject and source into a list
+    with tempfile.TemporaryDirectory() as temp_dir_name:
+        logging.info('writing sequences to %s', temp_dir_name)
+        temp_handles = {}
+        temp_writers = {}
+        for filename in args.seq_record_filenames:
+            logging.info('loading sequence records from %s', filename)
+            with open_compressed(filename, 'rb') as seq_record_handle:
+                seq_record_reader = fastavro.reader(seq_record_handle)
+                for record in seq_record_reader:
+                    subject = record['subject']
+                    source  = record['source']
+                    
+                    if args.no_none and source is None:
+                        continue
+                    elif args.only_none and source is not None:
+                        continue
 
-        logging.info('loading sequence records into memory')
+                    if subject not in temp_handles:
+                        temp_handles[subject] = {}
+                        temp_writers[subject] = {}
+                    if source not in temp_handles[subject]:
+                        temp_handles[subject][source] = open(os.path.join(temp_dir_name, f'subject={subject},source={source}.avro'), 'wb')
+                        temp_writers[subject][source] = open_avro(temp_handles[subject][source], seq_record_reader.writer_schema)
+                    temp_writers[subject][source].write(record)
 
-        seq_records = list(seq_record_reader)
-
-        logging.info('sorting %d sequence records', len(seq_records))
-
-        seq_records.sort(key=lambda x: (x['subject'], x['sample'], x['source'], x['name']))
-
+        # flush and close writers and handles
+        for subject in temp_handles:
+            for source in temp_handles[subject]:
+                temp_writers[subject][source].flush()
+                temp_handles[subject][source].close()
+        del temp_writers
+        
         logging.info('writing output')
-        fastavro.writer(sys.stdout.buffer, seq_record_reader.writer_schema, seq_records, codec='bzip2')
+
+        #
+        for subject in temp_handles:
+            subject_path = os.path.join(base_path, f'subject={subject}')
+            # make sure the subject directory is create
+            if os.path.isdir(subject_path):
+                logging.info('using existing subject directory %s and adding to it', subject_path)
+            else:
+                logging.info('making subject directory %s', subject_path)
+                os.mkdir(subject_path)
+
+            # for each (subject, source)
+            for source in temp_handles[subject]:
+                logging.info('loading and sorting records subject=%s/source=%s', subject, source)
+                with open(os.path.join(temp_dir_name, f'subject={subject},source={source}.avro'), 'rb') as input_handle:
+                    reader = fastavro.reader(input_handle)
+                    records = list(reader)
+                records.sort(key=itemgetter('name'))
+
+                logging.info('writing records subject=%s/source=%s', subject, source)
+                output_filename = os.path.join(base_path, f'subject={subject}', f'source={source}.avro')
+                with open(output_filename, 'wb') as output_handle:
+                    fastavro.writer(output_handle, reader.writer_schema, records, codec='bzip2')
+                del records
 
     elapsed_time = time.time() - start_time
     logging.info('elapsed time %s', time.strftime('%H hours, %M minutes, %S seconds', time.gmtime(elapsed_time)))
