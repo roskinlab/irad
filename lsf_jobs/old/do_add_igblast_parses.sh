42b6c186af3e50c93d06fde4d232c8685a0c52c6
Krishna Roskin <krishnaroskin@gmail.com>
2020-08-09 14:32:35 -0400

    some old code that might be useful later

--- lsf_jobs/old/do_add_igblast_parses.sh
+++ lsf_jobs/old/do_add_igblast_parses.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PARSER=/data/RoskinLab/irbase/pipeline/parse_igblast.py
+PARSER=~/irbase/pipeline/parse_igblast.py
 
 BATCH_NUMBER=${1?the six digit batch number must be given}
 if [ ${#BATCH_NUMBER} -ne 6 ] ; then
@@ -16,6 +16,6 @@ cat <<EOF
 #BSUB -L /bin/bash
 #BSUB -J add_igblast_${BATCH_NUMBER}
 
-${PARSER} ${DB_ROOT}/${SPECIES}_gl_{V,D,J} batch${BATCH_NUMBER}.records.avro batch${BATCH_NUMBER}.igblast.gz \
-    ${PARSE_LABEL} >batch${BATCH_NUMBER}.records_igblast.avro
+${PARSER} ${PARSE_LABEL} ${DB_ROOT}/${SPECIES}_gl_{V,D,J} batch${BATCH_NUMBER}.records.avro \
+    batch${BATCH_NUMBER}.igblast.gz >batch${BATCH_NUMBER}.records_igblast.avro
 EOF
--- lsf_jobs/old/do_igblast_fasta.sh
+++ lsf_jobs/old/do_igblast_fasta.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-BATCH_NUMBER=${1?the six digit batch number must be given}
-if [ ${#BATCH_NUMBER} -ne 6 ] ; then
-    echo "batch number must be 6 digits"
-    exit
-fi
+FASTA_FILE=${1?the input FASTA file must be given}
 SPECIES=${2-human}
 CHAIN=${3-ig}
 SUBCHAIN=${4-_h}
@@ -12,16 +8,17 @@ SUBCHAIN=${4-_h}
 IGDATA=/data/RoskinLab/programs/igblast_release/
 NUM_ALIGN=3
 DB_ROOT=/data/RoskinLab/data/imgt
+OUTPUT_FILE=${FASTA_FILE%.fasta}
+OUTPUT_FILE=${OUTPUT_FILE%.fa}
 
 cat <<EOF
 export IGDATA=${IGDATA}
 
-~/irbase/pipeline/fastq_to_fasta.py -t batch${BATCH_NUMBER}.merged_fq.gz \
-| /data/RoskinLab/programs/igblast_release/igblastn \
+/data/RoskinLab/programs/igblast_release/igblastn \
     -germline_db_V ${DB_ROOT}/${SPECIES}_gl_V -num_alignments_V ${NUM_ALIGN} -germline_db_V_seqidlist ${DB_ROOT}/${SPECIES}_${CHAIN^^}${SUBCHAIN^^}_ids \
     -germline_db_D ${DB_ROOT}/${SPECIES}_gl_D -num_alignments_D ${NUM_ALIGN} -germline_db_D_seqidlist ${DB_ROOT}/${SPECIES}_${CHAIN^^}${SUBCHAIN^^}_ids \
     -germline_db_J ${DB_ROOT}/${SPECIES}_gl_J -num_alignments_J ${NUM_ALIGN} -germline_db_J_seqidlist ${DB_ROOT}/${SPECIES}_${CHAIN^^}${SUBCHAIN^^}_ids \
     -organism ${SPECIES} -ig_seqtype ${CHAIN^} -num_clonotype 0 \
     -auxiliary_data /data/RoskinLab/programs/igblast_release/optional_file/${SPECIES}_gl.aux \
-    -query - | gzip >batch${BATCH_NUMBER}.igblast.gz
+    -query ${FASTA_FILE} | gzip >${OUTPUT_FILE}.igblast.gz
 EOF
