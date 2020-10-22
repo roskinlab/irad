e6a635fa1a6366a16e6470fd84e8d3eaaba5b1bc
Krishna Roskin <krishnaroskin@gmail.com>
2020-10-22 13:49:30 -0400

    now pass list of files to processes

--- lsf_jobs/do_cdr3_length.sh
+++ lsf_jobs/do_cdr3_length.sh
@@ -2,12 +2,28 @@
 
 CDR3_LENGTH=~/irbase/analysis/cdr3_length.py
 
+OUTPUT_FILE=${1?please provide the output file}
+shift
 PARSE_LABEL=${1?please provide the parser label to use}
-LINEAGE_LABEL=${2?please provide the lineage label to use}
+shift
+LINEAGE_LABEL=${1?please provide the lineage label to use}
+shift
+
+# make sure the input files exist
+for f in "$@" ; do
+    if [ ! -f "$f" ]; then
+        echo >&2 ${f} does not exists
+        exit 1
+    fi
+done
 
 cat <<EOS
 #BSUB -L /bin/bash
 #BSUB -W 2:00
 
-${CDR3_LENGTH} ${PARSE_LABEL} --lineage ${LINEAGE_LABEL} source=*.clones.igblast.avro >cdr3_length.csv
+${CDR3_LENGTH} ${PARSE_LABEL} --lineage ${LINEAGE_LABEL} >${OUTPUT_FILE} \\
 EOS
+for f in "$@" ; do
+    echo \"${f}\" \\
+done
+echo
